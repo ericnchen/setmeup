90c2068e183470339eb451a3762ecb83dd5b0e74
Eric Chen <eric@ericnchen.com>
2019-03-10 13:31:31 +0100

    Add some docstrings and move dotfile main function up to top.

--- setmeup/dotfiles.py
+++ setmeup/dotfiles.py
@@ -15,7 +15,24 @@ DOTFILES = (
 )
 
 
+def main():
+    dotfiles = get_dotfiles_to_install()
+
+    print("\nThe following dotfiles will be installed:\n")
+    for src, tar in dotfiles:
+        print("\t {} to {}".format(src, tar))
+
+    if raw_input("\nContinue? ").lower() not in ("yes", "y"):
+        return
+
+    for src, tar in dotfiles:
+        print("\nInstalling {} to {} ...".format(src, tar))
+        shutil.copy2(os.path.abspath(os.sep.join([ASSETDIR, src])), tar)
+        print("Installing {} to {} ... done.".format(src, tar))
+
+
 def read_target_path(filepath):
+    """Read the first line of a dotfile to see where it should be placed."""
     with open(filepath, "r") as f:
         first_line = f.readline()  # e.g.: # ~/.bashrc
     return first_line.split(" ")[1].rstrip()  # remove comment marker and trailing \n
@@ -26,6 +43,11 @@ def file_exists(filepath):
 
 
 def get_dotfiles_to_install():
+    """Gather up a list of which dotfiles need to be installed.
+
+    Dotfiles that don't already exist in the target path will be automatically
+    added to the list. Dotfiles that do exist need to be manually confirmed.
+    """
     dotfiles_to_install = []
     for fn in DOTFILES:
         fn_target = os.path.expanduser(read_target_path(os.sep.join([ASSETDIR, fn])))
@@ -35,22 +57,6 @@ def get_dotfiles_to_install():
                 continue
         dotfiles_to_install.append((fn, fn_target))
     return dotfiles_to_install
-
-
-def main():
-    dotfiles = get_dotfiles_to_install()
-
-    print("\nThe following dotfiles will be installed:\n")
-    for src, tar in dotfiles:
-        print("\t {} to {}".format(src, tar))
-
-    if input("\nContinue? ").lower() not in ("yes", "y"):
-        return
-
-    for src, tar in dotfiles:
-        print("\nInstalling {} to {} ...".format(src, tar))
-        shutil.copy2(os.path.abspath(os.sep.join([ASSETDIR, src])), tar)
-        print("Installing {} to {} ... done.".format(src, tar))
 
 
 if __name__ == "__main__":
