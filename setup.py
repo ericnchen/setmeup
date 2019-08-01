746fa9cee77664d0e43efec423c047abe59170f9
Eric Chen <eric@ericnchen.com>
2019-08-01 09:44:10 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="dtf",
-    version="0.1.3",
+    version="0.1.4",
     description="dtf is my personal dotfile manager.",
     url="https://github.com/ericnchen/dtf",
     author="Eric Chen",
